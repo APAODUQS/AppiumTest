494e050c16bc9f5dd8dd3f4ea22a9ca6c80d8620
Paola Duquino <angela.duquino@appgate.com>
2020-10-30 15:54:50 -0500

    Fix sh

--- src/test/java/test.sh
+++ src/test/java/test.sh
@@ -1,6 +1,7 @@
-adb install -r ./apk/app.apk
+#!/bin/sh
+#adb install -r ./apk/app.apk
 source ~/.bash_profile
 appium --address 127.0.0.1 --port 4723 --default-capabilities '{"deviceName": "Pixel 3 API 28", "udid": "emulator-5554", "platformName": "Android", "appPackage": "com.easysolutions.sdk.test", "appActivity": ".SplashActivity"}' &
 sleep 3
 mvn test
-adb uninstall com.easysolutions.sdk.test
+#adb uninstall com.easysolutions.sdk.test
